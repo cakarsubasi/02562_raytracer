a485072a37a72a4201563783311cdf4d7aa8aec2
Cem Akarsubasi <cemakarsubasi@gmail.com>
2023-10-29 15:16:09 +0100

    Add shadertype enum

--- src/command.rs
+++ src/command.rs
@@ -1,9 +1,25 @@
 use winit::{dpi::PhysicalSize, event::{VirtualKeyCode, ElementState}};
+use strum_macros::{EnumIter, IntoStaticStr};
 
+#[derive(Debug)]
 pub enum Command {
     Resize { new_size: PhysicalSize<u32> },
     Render { value: bool },
     LoadShader { shader_path: String },
+    SetCameraConstant { constant: f32 },
+    SetSphereMaterial { material: ShaderType },
+    SetOtherMaterial { material: ShaderType },
     KeyEvent {key: VirtualKeyCode, state: ElementState },
     Shutdown { value: bool },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, EnumIter, IntoStaticStr)]
+pub enum ShaderType {
+    Lambertian = 0,
+    Phong = 1,
+    Mirror = 2,
+    Transmit = 3,
+    Glossy = 4,
+    Normal = 5,
+    BaseColor = 6,
 }
